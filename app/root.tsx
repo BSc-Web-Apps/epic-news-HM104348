06b7125ab6c75384d262641ab1b8e4a55d21beaf
HM104348 <115796125+HM104348@users.noreply.github.com>
2024-11-14 15:30:28 +0000

    Text, Button, Background
    I changed all of the 3

--- app/root.tsx
+++ app/root.tsx
@@ -2,7 +2,6 @@ import { type LinksFunction } from '@remix-run/node'
 import Document from '~/components/shared-layout/Document'
 import { useNonce } from '~/utils/nonce-provider.ts'
 import rootLinkElements from '~/utils/providers/rootLinkElements'
-
 export const links: LinksFunction = () => {
 	return rootLinkElements
 }
@@ -13,11 +12,18 @@ export default function App() {
 	const nonce = useNonce()
 
 	return (
+
 		<Document nonce={nonce}>
-			<div className="flex h-screen flex-col justify-between">
+
+			<div className="flex h-screen flex-col justify-between bg-green-300">
 				<div className="flex-1">
 					<main className="grid h-full place-items-center">
-						<h1 className="text-mega">Welcome to Epic News!</h1>
+						<h1 className= "text-mega text-gray-700">Your Journey Begins!</h1>
+						<p className='text-base text-gray-700 md:text-lg lg:text-xl'>
+							Welcome to Epic News, where the latest developments in tech are found.</p>
+							<button className="bg-indigo-500 ... px-8 py-4 hover:bg-blue-400">
+ 								 Learn More
+						</button>
 					</main>
 				</div>
 			</div>
