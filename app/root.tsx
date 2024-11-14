3f4615e618a1d8c4f21ea64dc739d465b7532df4
HM104348 <115796125+HM104348@users.noreply.github.com>
2024-11-14 15:56:44 +0000

    Header, Footer, Logo
    Added those 3

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,14 @@
 import { type LinksFunction } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 import Document from '~/components/shared-layout/Document'
+import ThemeSwitch from '~/components/shared-layout/ThemeSwitch'
 import { useNonce } from '~/utils/nonce-provider.ts'
 import rootLinkElements from '~/utils/providers/rootLinkElements'
+import { type loader } from './__root.server'
+import FooterMenuRight from './components/organisms/Footer/FooterMenuRight'
+import HeaderWithSearch from './components/organisms/HeaderWithSearch'
+import useTheme from './hooks/useTheme.tsx'
+
 export const links: LinksFunction = () => {
 	return rootLinkElements
 }
@@ -9,23 +16,31 @@ export { headers, meta } from './__root.client.tsx'
 export { action, loader } from './__root.server.tsx'
 
 export default function App() {
+	const data = useLoaderData<typeof loader>();
 	const nonce = useNonce()
-
+	const theme = useTheme()
 	return (
 
-		<Document nonce={nonce}>
+		<Document nonce={nonce} theme={theme}>
 
-			<div className="flex h-screen flex-col justify-between bg-green-300">
+			<div className="flex h-screen flex-col justify-between">
+			<HeaderWithSearch />
 				<div className="flex-1">
 					<main className="grid h-full place-items-center">
-						<h1 className= "text-mega text-gray-700">Your Journey Begins!</h1>
-						<p className='text-base text-gray-700 md:text-lg lg:text-xl'>
+						<h1 className= "text-mega text-red-700">Your Journey Begins!</h1>
+						<p className='text-base text-red-700 md:text-lg lg:text-xl'>
 							Welcome to Epic News, where the latest developments in tech are found.</p>
 							<button className="bg-indigo-500 ... px-8 py-4 hover:bg-blue-400">
  								 Learn More
 						</button>
 					</main>
 				</div>
+				
+				<div className="container flex justify-between pb-5">
+          <ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
+        </div>
+		
+        <FooterMenuRight />
 			</div>
 		</Document>
 	)
