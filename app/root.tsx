da42fd66962bc492a6c1ed6593ed613d8df1557f
HM104348 <115796125+HM104348@users.noreply.github.com>
2024-12-05 14:47:57 +0000

    Finished Parallax Background

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,8 @@
 import { type LinksFunction } from '@remix-run/node'
-import { Link, useLoaderData } from '@remix-run/react'
+import { Outlet, useLoaderData } from '@remix-run/react'
 import { ParallaxProvider } from 'react-scroll-parallax'
 import heroImage from '~/assets/jpg/sample-hero.jpg'
+import logo from '~/assets/svg/logo.svg'
 import Document from '~/components/shared-layout/Document'
 import ThemeSwitch from '~/components/shared-layout/ThemeSwitch'
 import { useNonce } from '~/utils/nonce-provider.ts'
@@ -11,13 +12,13 @@ import { Button } from './components/atoms/Button.tsx'
 import FooterMenuRight from './components/organisms/Footer/FooterMenuRight'
 import HeaderWithSearch from './components/organisms/HeaderWithSearch'
 import HeroCallToAction from './components/organisms/Hero/HeroCallToAction.tsx'
+import ParallaxBackground from './components/organisms/Hero/ParallaxBackground.tsx'
 import useTheme from './hooks/useTheme.tsx'
 export const links: LinksFunction = () => {
 	return rootLinkElements
 }
 export { headers, meta } from './__root.client.tsx'
 export { action, loader } from './__root.server.tsx'
-
 export default function App() {
 	const data = useLoaderData<typeof loader>()
 	const nonce = useNonce()
@@ -28,7 +29,7 @@ export default function App() {
 				<div className="flex h-screen flex-col justify-between">
 					<HeaderWithSearch />
 					<div className="flex-1">
-						<main className="h-full">
+						{/* <main className="h-full">
 							<HeroCallToAction
 								image={heroImage}
 								imageRight={true}
@@ -48,10 +49,32 @@ export default function App() {
 								</div>
 							</HeroCallToAction>
 
+							<ParallaxBackground
+								image={heroImage}
+								title="Epic News"
+								logo={logo}
+								altText="Welcome to Epic News, where the latest developments in tech are found."
+							>
+								<div className="mx-auto flex w-fit flex-1 flex-col justify-between gap-16 bg-secondary/40 px-28 py-16 backdrop-blur-sm">
+									<p className="text-center text-4xl font-extrabold text-secondary-foreground">
+										The latest tech news in one place
+									</p>
+									<div className="flex justify-center gap-8">
+										<Button variant="default" size="wide">
+											<Link to="/signup">Sign up</Link>
+										</Button>
+										<Button variant="secondary" size="wide">
+											<Link to="/login">Login</Link>
+										</Button>
+									</div>
+								</div>
+							</ParallaxBackground>
+
 							<button className="... bg-indigo-500 px-8 py-4 hover:bg-blue-400">
 								Learn More
 							</button>
-						</main>
+						</main> */}
+						<Outlet />
 					</div>
 
 					<div className="container flex justify-between pb-5">
