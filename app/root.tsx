9fc9e5f7478061219bb51f9c1a916e6842226da4
HM104348 <115796125+HM104348@users.noreply.github.com>
2024-12-05 16:14:04 +0000

    Move home page content to _index.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,13 @@
 import { type LinksFunction } from '@remix-run/node'
 import { Outlet, useLoaderData } from '@remix-run/react'
 import { ParallaxProvider } from 'react-scroll-parallax'
-import heroImage from '~/assets/jpg/sample-hero.jpg'
-import logo from '~/assets/svg/logo.svg'
 import Document from '~/components/shared-layout/Document'
 import ThemeSwitch from '~/components/shared-layout/ThemeSwitch'
 import { useNonce } from '~/utils/nonce-provider.ts'
 import rootLinkElements from '~/utils/providers/rootLinkElements'
 import { type loader } from './__root.server'
-import { Button } from './components/atoms/Button.tsx'
 import FooterMenuRight from './components/organisms/Footer/FooterMenuRight'
 import HeaderWithSearch from './components/organisms/HeaderWithSearch'
-import HeroCallToAction from './components/organisms/Hero/HeroCallToAction.tsx'
-import ParallaxBackground from './components/organisms/Hero/ParallaxBackground.tsx'
 import useTheme from './hooks/useTheme.tsx'
 export const links: LinksFunction = () => {
 	return rootLinkElements
@@ -29,52 +24,6 @@ export default function App() {
 				<div className="flex h-screen flex-col justify-between">
 					<HeaderWithSearch />
 					<div className="flex-1">
-						{/* <main className="h-full">
-							<HeroCallToAction
-								image={heroImage}
-								imageRight={true}
-								hasBackgroundColour={true}
-							>
-								<div className="flex h-full flex-1 flex-col justify-between p-16">
-									<div className="flex flex-col gap-8">
-										<h2 className="text-h2">Welcome to Epic News</h2>
-										<p className="text-lg">
-											Keep up to date with the latest tech news.
-										</p>
-									</div>
-
-									<Button asChild variant="default" size="lg">
-										<Link to="/signup">Sign up</Link>
-									</Button>
-								</div>
-							</HeroCallToAction>
-
-							<ParallaxBackground
-								image={heroImage}
-								title="Epic News"
-								logo={logo}
-								altText="Welcome to Epic News, where the latest developments in tech are found."
-							>
-								<div className="mx-auto flex w-fit flex-1 flex-col justify-between gap-16 bg-secondary/40 px-28 py-16 backdrop-blur-sm">
-									<p className="text-center text-4xl font-extrabold text-secondary-foreground">
-										The latest tech news in one place
-									</p>
-									<div className="flex justify-center gap-8">
-										<Button variant="default" size="wide">
-											<Link to="/signup">Sign up</Link>
-										</Button>
-										<Button variant="secondary" size="wide">
-											<Link to="/login">Login</Link>
-										</Button>
-									</div>
-								</div>
-							</ParallaxBackground>
-
-							<button className="... bg-indigo-500 px-8 py-4 hover:bg-blue-400">
-								Learn More
-							</button>
-						</main> */}
-
 						<Outlet />
 					</div>
 
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,59 @@
 import { type MetaFunction } from '@remix-run/node'
+import { Link } from '@remix-run/react'
+import HeroCallToAction from '#app/components/organisms/Hero/HeroCallToAction.tsx'
+import ParallaxBackground from '#app/components/organisms/Hero/ParallaxBackground.tsx'
+import { Button } from '#app/components/ui/button.tsx'
+import heroImage from '~/assets/jpg/sample-hero.jpg'
+import logo from '~/assets/svg/logo.svg'
 
 export const meta: MetaFunction = () => [{ title: 'Epic News' }]
 
 export default function Index() {
 	return (
-		<main className="grid h-full place-items-center">
-			<h1 className="text-mega">
-				Hello from{' '}
-				<pre className="prose rounded-lg bg-primary p-6 text-primary-foreground">
-					app/routes/_index.tsx
-				</pre>
-			</h1>
+		<main className="h-full">
+			<HeroCallToAction
+				image={heroImage}
+				imageRight={true}
+				hasBackgroundColour={true}
+			>
+				<div className="flex h-full flex-1 flex-col justify-between p-16">
+					<div className="flex flex-col gap-8">
+						<h2 className="text-h2">Welcome to Epic News</h2>
+						<p className="text-lg">
+							Keep up to date with the latest tech news.
+						</p>
+					</div>
+
+					<Button asChild variant="default" size="lg">
+						<Link to="/signup">Sign up</Link>
+					</Button>
+				</div>
+			</HeroCallToAction>
+
+			<ParallaxBackground
+				image={heroImage}
+				title="Epic News"
+				logo={logo}
+				altText="Welcome to Epic News, where the latest developments in tech are found."
+			>
+				<div className="mx-auto flex w-fit flex-1 flex-col justify-between gap-16 bg-secondary/40 px-28 py-16 backdrop-blur-sm">
+					<p className="text-center text-4xl font-extrabold text-secondary-foreground">
+						The latest tech news in one place
+					</p>
+					<div className="flex justify-center gap-8">
+						<Button variant="default" size="wide">
+							<Link to="/signup">Sign up</Link>
+						</Button>
+						<Button variant="secondary" size="wide">
+							<Link to="/login">Login</Link>
+						</Button>
+					</div>
+				</div>
+			</ParallaxBackground>
+
+			<button className="... bg-indigo-500 px-8 py-4 hover:bg-blue-400">
+				Learn More
+			</button>
 		</main>
 	)
 }
