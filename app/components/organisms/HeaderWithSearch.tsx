a6964611ad0eef8341da2a818e0384d1c0cc8a7d
HM104348 <115796125+HM104348@users.noreply.github.com>
2024-12-05 15:09:07 +0000

    Added about us tab and added link to it via nav bar

--- app/components/organisms/HeaderWithSearch.tsx
+++ app/components/organisms/HeaderWithSearch.tsx
@@ -14,12 +14,19 @@ export default function HeaderWithSearch() {
 				<Link to="/">
 					<NavLogo />
 				</Link>
-				<div>
+				<div className="flex flex-1 justify-center gap-8">
 					<Link
 						to="/news"
 						className="text-sm font-semibold text-muted-foreground transition hover:text-foreground"
 					>
 						News
+					</Link>
+					<Link
+						to="/about-us"
+						prefetch="intent"
+						className="text-sm font-semibold text-muted-foreground transition hover:text-foreground"
+					>
+						About us
 					</Link>
 				</div>
 				<div className="ml-auto hidden max-w-sm flex-1 sm:block">
