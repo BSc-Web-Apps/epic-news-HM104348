d5cd960f9d05ad0e03a4c57b65fb75994c10c209
HM104348 <115796125+HM104348@users.noreply.github.com>
2024-12-12 09:20:07 +0000

    Imported useLoadData and toTitleCase

--- app/routes/news.$category.tsx
+++ app/routes/news.$category.tsx
@@ -1,13 +1,18 @@
 import { type LoaderFunctionArgs, json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { toTitleCase } from '#app/utils/stringUtils.ts'
 
 export async function loader({ params }: LoaderFunctionArgs) {
 	const { category } = params
+
+	const categoryTitle = toTitleCase(category)
 
 	console.log({ category })
 
 	return json({})
 }
 export default function NewsCategoryPage() {
+	const { categoryTitle } = useLoaderData<typeof loader>()
 	return (
 		<div className="container py-16">
 			<h2 className="text-h2">Generic news category page</h2>
