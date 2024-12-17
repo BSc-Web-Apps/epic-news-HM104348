0b348098143bddf7675acc4ccdcb812ef0f0945e
HM104348 <115796125+HM104348@users.noreply.github.com>
2024-12-17 17:39:33 +0000

    Attempted to Do extra credit

---
+++ app/components/organisms/ArticleCard.tsx
@@ -0,0 +1,6 @@
+import { img } from 
+
+								const key={article.id}
+								const title={article.title}
+								const category={article.category?.name}
+								const imageId={article.images[0]?.id}
