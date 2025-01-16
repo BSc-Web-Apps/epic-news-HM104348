72b4e87fb49c926792b87631ea4119f074444407
HM104348 <115796125+HM104348@users.noreply.github.com>
2025-01-16 13:41:54 +0000

    Admin review page added

--- app/routes/article.$articleId.tsx
+++ app/routes/article.$articleId.tsx
@@ -1,6 +1,7 @@
 import { invariant } from '@epic-web/invariant'
 import { type LoaderFunctionArgs, json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
+
 import { prisma } from '~/utils/db.server.ts'
 
 export async function loader({ params }: LoaderFunctionArgs) {
@@ -32,6 +33,7 @@ const ArticleNotFound = () => {
 		</div>
 	)
 }
+
 export default function ArticlePage() {
 	const { article } = useLoaderData<typeof loader>()
 
