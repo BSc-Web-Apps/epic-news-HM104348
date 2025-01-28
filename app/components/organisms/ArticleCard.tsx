413c70c80af20782e442874c676008d2a4be6337
HM104348 <115796125+HM104348@users.noreply.github.com>
2025-01-28 12:28:48 +0000

    Updated the icon and moved it to the bottom

--- app/components/organisms/ArticleCard.tsx
+++ app/components/organisms/ArticleCard.tsx
@@ -1,5 +1,5 @@
 import { Link } from '@remix-run/react'
-import { FaLeaf } from 'react-icons/fa'
+import { GiFallingLeaf } from 'react-icons/gi'
 
 interface ArticleCardProps {
 	articleId: string
@@ -17,14 +17,24 @@ export default function ArticleCard({
 	return (
 		<Link to={`/article/${articleId}`}>
 			<div>
-				<div className="display: flex; flex-direction: flex-col; line-clamp-6 flex h-40 flex-col justify-between rounded-lg bg-green-600 p-4 transition hover:scale-105 hover:bg-green-900">
+				<div
+					className="display: flex; 
+				flex-direction: flex-col; 
+				line-clamp-6 flex h-40 flex-col justify-between rounded-lg bg-green-600 p-4 transition hover:scale-105 hover:bg-green-900"
+				>
 					<div
-						style={{ display: 'flex', alignItems: 'top right', gap: '0.4rem' }}
+						style={{
+							display: 'flex',
+							alignItems: 'start',
+							gap: '0.4rem',
+						}}
 					>
-						<FaLeaf /> {}
-						<h3>{title}</h3>
+						<h3> {title}</h3>
 					</div>
-					<p>{category?.name || 'General News'}</p>
+					<p>
+						{' '}
+						<GiFallingLeaf size={20} /> {} {category?.name || 'General News'}{' '}
+					</p>
 				</div>
 			</div>
 		</Link>
