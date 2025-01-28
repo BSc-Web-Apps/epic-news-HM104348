84f5d222e691d4bed40f6ba79f6b27f059debf0f
HM104348 <115796125+HM104348@users.noreply.github.com>
2025-01-28 10:56:21 +0000

    Added Icon next to all articals of a leaf

--- app/components/organisms/ArticleCard.tsx
+++ app/components/organisms/ArticleCard.tsx
@@ -1,4 +1,5 @@
 import { Link } from '@remix-run/react'
+import { FaLeaf } from 'react-icons/fa'
 
 interface ArticleCardProps {
 	articleId: string
@@ -16,11 +17,13 @@ export default function ArticleCard({
 	return (
 		<Link to={`/article/${articleId}`}>
 			<div>
-				<div
-					className="display: flex; flex-direction: flex-col; line-clamp-6 flex h-40 flex-col justify-between rounded-lg
- bg-green-600 p-4 transition hover:scale-105 hover:bg-green-900"
-				>
-					<h3>{title}</h3>
+				<div className="display: flex; flex-direction: flex-col; line-clamp-6 flex h-40 flex-col justify-between rounded-lg bg-green-600 p-4 transition hover:scale-105 hover:bg-green-900">
+					<div
+						style={{ display: 'flex', alignItems: 'top right', gap: '0.4rem' }}
+					>
+						<FaLeaf /> {}
+						<h3>{title}</h3>
+					</div>
 					<p>{category?.name || 'General News'}</p>
 				</div>
 			</div>
